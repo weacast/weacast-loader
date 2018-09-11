447c417749420db4326c06c04a2cf8c2ccada915
Luc Claustres <luc.claustres@orange.fr>
2018-09-11 16:28:59 +0200

    Improved jobs

--- jobfile-arpege-world.js
+++ jobfile-arpege-world.js
@@ -1,25 +1,17 @@
-const job = require('./jobfile-arpege')
+const createJob = require('./jobfile-arpege')
 
-// Setup job name
-Object.assign(job, {
-  id: 'weacast-arpege-world'
+// Setup job name, model name, bounds and generation parameters
+module.exports = createJob({
+  id: 'weacast-arpege-world',
+  model: 'arpege-world',
+  subsets: {
+    long: [-180, 180],
+  	lat: [-90, 90]
+  },
+  nwp: {
+    runInterval: 6 * 3600,          // Produced every 6h
+    interval: 3 * 3600,             // Steps of 3h
+    lowerLimit: 0 * 3600,           // From T0
+    upperLimit: 3 * 3600,           // Up to T0 + 3h for testing
+  }
 })
-
-// Setup model name and bounds
-Object.assign(job.taskTemplate, {
-  model: 'arpege-world'
-})
-Object.assign(job.taskTemplate.options.subsets, {
-  long: [-180, 180],
-  lat: [-90, 90]
-})
-
-// Setup model generation parameters
-Object.assign(job.hooks.jobs.before.generateNwpTasks, {
-  runInterval: 6 * 3600,          // Produced every 6h
-  interval: 3 * 3600,             // Steps of 3h
-  lowerLimit: 0 * 3600,           // From T0
-  upperLimit: 3 * 3600,           // Up to T0 + 3h for testing
-})
-
-module.exports = job
--- jobfile-arpege.js
+++ jobfile-arpege.js
@@ -1,94 +1,110 @@
 const path = require('path')
 const fs = require('fs')
 
-module.exports = {
+const defaults = {
   id: 'weacast-arpege',
-  store: 'fs',
-  options: {
-    workersLimit: 4,
-    faultTolerant: true
-  },
-  taskTemplate: {
-    //id: 'arpege/<%= element %>/<%= level %>/<%= forecastTime.format(\'YYYY-MM-DD[_]HH-mm-ss\') %>',
-    id: 'arpege/<%= element %>/<%= level %>/<%= timeOffset / 3600 %>',
-    type: 'wcs',
-    // Common options for models, some will be setup on a per-model basis
+  model: 'arpege',
+  request: {},
+  subsets: {},
+  nwp: {},
+  elements: [{
+    element: 'u-wind',
+    name: 'U_COMPONENT_OF_WIND__SPECIFIC_HEIGHT_LEVEL_ABOVE_GROUND',
+    levels: [ 10 ]
+  }, {
+    element: 'v-wind',
+    name: 'V_COMPONENT_OF_WIND__SPECIFIC_HEIGHT_LEVEL_ABOVE_GROUND',
+    levels: [ 10 ]
+  }, {
+    element: 'gust',
+    name: 'WIND_SPEED_GUST__SPECIFIC_HEIGHT_LEVEL_ABOVE_GROUND',
+    levels: [ 10 ]
+  }]
+}
+
+module.exports = (options) => {
+  options = Object.assign(defaults, options)
+
+  return {
+    id: options.id,
+    store: 'fs',
     options: {
-      url: 'https://geoservices.meteofrance.fr/services/MF-NWP-GLOBAL-ARPEGE-05-GLOBE-WCS',
-      version: '2.0.1',
-      token: '__qEMDoIC2ogPRlSoRQLGUBOomaxJyxdEd__',
-      coverageid: '<%= name %>___<%= runTime.format() %>',
-      subsets: {
-        time: '<%= forecastTime.format() %>',
-        height: '<%= level %>'
-      }
-    }
-  },
-  hooks: {
-    tasks: {
-      before: {
-        createMongoCollection: {
-          collection: '<%= model %>-<%= element %>',
-          index: [{ forecastTime: 1 }, { expireAfterSeconds: 3 * 3600 }]
-        },
-        readMongoCollection: {
-          collection: '<%= model %>-<%= element %>', dataPath: 'data.previousData',
-          query: { forecastTime: '<%= forecastTime.format() %>' },
-          project: { _id: 1, runTime: 1, forecastTime: 1 }
-        },
-        // Do not download data if already here
-        discardIf: { 'previousData.runTime': '<%= runTime.format() %>' }
-      },
-      after: {
-        runCommand: {
-          command: 'weacast-gtiff2json ./output/<%= id %> -o ./output/<%= id %>.json'
-        },
-        // This will add grid data in a data field
-        readJson: {
-          key: '<%= id %>.json'
-        },
-        transformJson: { dataPath: 'result', pick: ['id', 'model', 'element', 'level', 'runTime', 'forecastTime', 'data'] },
-        writeMongoCollection: { dataPath: 'result', collection: '<%= model %>-<%= element %>', transform: { omit: ['id', 'model', 'element'] } },
-        clearData: { dataPath: 'result.data' } // This will free memory for grid data
-      }
+      workersLimit: 4,
+      faultTolerant: true
     },
-    jobs: {
-      before: {
-        createStores: [{
-          id: 'fs',
-          options: {
-            path: path.join(__dirname, 'output')
-          }
-        }],
-        connectMongo: {
-          url: 'mongodb://127.0.0.1:27017/weacast',
-          // Required so that client is forwarded from job to tasks
-          clientPath: 'taskTemplate.client'
+    taskTemplate: {
+      //id: 'arpege/<%= element %>/<%= level %>/<%= forecastTime.format(\'YYYY-MM-DD[_]HH-mm-ss\') %>',
+      id: `${options.model}/<%= element %>/<%= level %>/<%= timeOffset / 3600 %>`,
+      type: 'wcs',
+      // Common options for models, some will be setup on a per-model basis
+      options: Object.assign({
+        url: 'https://geoservices.meteofrance.fr/services/MF-NWP-GLOBAL-ARPEGE-05-GLOBE-WCS',
+        version: '2.0.1',
+        token: '__qEMDoIC2ogPRlSoRQLGUBOomaxJyxdEd__',
+        coverageid: '<%= name %>___<%= runTime.format() %>',
+        subsets: Object.assign({
+          time: '<%= forecastTime.format() %>',
+          height: '<%= level %>'
+        }, options.subsets)
+      }, options.request)
+    },
+    hooks: {
+      tasks: {
+        before: {
+          readMongoCollection: {
+            collection: '<%= model %>-<%= element %>', dataPath: 'data.previousData',
+            query: { forecastTime: '<%= forecastTime.format() %>' },
+            project: { _id: 1, runTime: 1, forecastTime: 1 }
+          },
+          // Do not download data if already here
+          discardIf: { 'previousData.runTime': '<%= runTime.format() %>' }
         },
-        // Common options for models, some will be setup on a per-model basis
-        generateNwpTasks: {
-          runIndex: -1, // -1 is not current run but previous one to ensure it is already available
-          elements: [{
-            element: 'u-wind',
-            name: 'U_COMPONENT_OF_WIND__SPECIFIC_HEIGHT_LEVEL_ABOVE_GROUND',
-            levels: [ 10 ]
-          }, {
-            element: 'v-wind',
-            name: 'V_COMPONENT_OF_WIND__SPECIFIC_HEIGHT_LEVEL_ABOVE_GROUND',
-            levels: [ 10 ]
-          }, {
-            element: 'gust',
-            name: 'WIND_SPEED_GUST__SPECIFIC_HEIGHT_LEVEL_ABOVE_GROUND',
-            levels: [ 10 ]
-          }]
+        after: {
+          runCommand: {
+            command: 'weacast-gtiff2json ./output/<%= id %> -o ./output/<%= id %>.json'
+          },
+          // This will add grid data in a data field
+          readJson: {
+            key: '<%= id %>.json'
+          },
+          transformJson: { dataPath: 'result', pick: ['id', 'model', 'element', 'level', 'runTime', 'forecastTime', 'data'] },
+          writeMongoCollection: { dataPath: 'result', collection: '<%= model %>-<%= element %>', transform: { omit: ['id', 'model', 'element'] } },
+          clearData: { dataPath: 'result.data' } // This will free memory for grid data
         }
       },
-      after: {
-        disconnectMongo: {
-          clientPath: 'taskTemplate.client'
+      jobs: {
+        before: {
+          createStores: [{
+            id: 'fs',
+            options: {
+              path: path.join(__dirname, 'output')
+            }
+          }],
+          connectMongo: {
+            url: 'mongodb://127.0.0.1:27017/weacast',
+            // Required so that client is forwarded from job to tasks
+            clientPath: 'taskTemplate.client'
+          },
+          parallel: options.elements.map(item => ({
+            hook: 'createMongoCollection',
+            collection: `${options.model}-${item.element}`,
+            index: [{ forecastTime: 1 }, { expireAfterSeconds: options.nwp.interval }],
+            // Required so that client is forwarded from job to tasks
+            clientPath: 'taskTemplate.client'
+          })),
+          // Common options for models, some will be setup on a per-model basis
+          generateNwpTasks: Object.assign({
+            runIndex: -1, // -1 is not current run but previous one to ensure it is already available
+            elements: options.elements.map(element => Object.assign({ model: options.model }, element))
+          }, options.nwp)
         },
-        clearOutputs: {},
-        removeStores: ['fs']
+        after: {
+          disconnectMongo: {
+            clientPath: 'taskTemplate.client'
+          },
+          clearOutputs: {},
+          removeStores: ['fs']
+        }
       }
     }
   }
