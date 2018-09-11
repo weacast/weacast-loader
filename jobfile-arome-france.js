ff38c755745310574b15574efb35d8da701a4539
Luc Claustres <luc.claustres@orange.fr>
2018-09-11 17:03:48 +0200

    Added ARPEGE EUROPE & AROME FRANCE

---
+++ jobfile-arome-france.js
@@ -0,0 +1,22 @@
+// ARPEGE/AROME interfaces are similar
+const createJob = require('./jobfile-arpege')
+
+// Setup job name, model name, bounds and generation parameters
+module.exports = createJob({
+  id: 'weacast-arpege-arome',
+  model: 'arome-france',
+  request: {
+    url: 'https://geoservices.meteofrance.fr/services/MF-NWP-HIGHRES-AROME-0025-FRANCE-WCS'
+  },
+  subsets: {
+    long: [-8, 12],
+  	lat: [38, 53]
+  },
+  nwp: {
+    runInterval: 3 * 3600,            // Produced every 3h
+    interval: 1 * 3600,               // Steps of 1h
+    lowerLimit: 0,                    // From T0
+    //upperLimit: 42 * 3600,            // Up to T0+42
+    upperLimit: 3 * 3600              // Up to T0 + 3h for testing
+  }
+})
