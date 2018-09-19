bcd1198e44ae28388b761ef7ea2095b1a51e7826
Luc Claustres <luc.claustres@orange.fr>
2018-09-19 14:24:06 +0200

    Added docker build with latest tag in Travis

--- travis.build.sh
+++ travis.build.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 source travis.env.sh
-# Build docker only on release
-if [[ -z "$TRAVIS_TAG" ]]
-then
-	echo "Skipping docker images build"
-else
-	docker build -f dockerfile.arpege -t weacast/weacast-arpege .
-	docker tag weacast/weacast-arpege weacast/weacast-arpege:$VERSION
-	docker build -f dockerfile.gfs -t weacast/weacast-gfs .
-	docker tag weacast/weacast-gfs weacast/weacast-gfs:$VERSION
-	docker login -u="$DOCKER_USER" -p="$DOCKER_PASSWORD"
-	docker push weacast/weacast-arpege:$VERSION
-	docker push weacast/weacast-gfs:$VERSION
-fi
+
+docker build -f dockerfile.arpege -t weacast/weacast-arpege .
+docker tag weacast/weacast-arpege weacast/weacast-arpege:$VERSION
+docker build -f dockerfile.gfs -t weacast/weacast-gfs .
+docker tag weacast/weacast-gfs weacast/weacast-gfs:$VERSION
+docker login -u="$DOCKER_USER" -p="$DOCKER_PASSWORD"
+docker push weacast/weacast-arpege:$VERSION
+docker push weacast/weacast-gfs:$VERSION
--- travis.env.sh
+++ travis.env.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-export VERSION=$(node -p -e "require('./package.json').version")
+
+# Build docker with version number only on release
+if [[ -z "$TRAVIS_TAG" ]]
+then
+	export VERSION=$(node -p -e "require('./package.json').version")
+else
+	export VERSION=latest
+fi
