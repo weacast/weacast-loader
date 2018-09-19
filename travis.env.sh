47bed6cbe58a4b4161aeddccc50ffba32cff89b0
Luc Claustres <luc.claustres@orange.fr>
2018-09-19 14:52:03 +0200

    Fixed travis docker build

--- travis.env.sh
+++ travis.env.sh
@@ -3,7 +3,7 @@
 # Build docker with version number only on release
 if [[ -z "$TRAVIS_TAG" ]]
 then
-	export VERSION=$(node -p -e "require('./package.json').version")
-else
 	export VERSION=latest
+else
+	export VERSION=$(node -p -e "require('./package.json').version")
 fi
