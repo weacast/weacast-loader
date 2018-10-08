1025675b890a95ea4e9928ffc0cccb7dd2ea7568
Luc Claustres <luc.claustres@orange.fr>
2018-10-08 16:37:19 +0200

    Simplified docker files to work similar in dev or release (based on krawler version in package.json)

--- travis.build.sh
+++ travis.build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 source travis.env.sh
 
-docker build -f dockerfile.arpege -t weacast/weacast-arpege .
+docker build --build-arg KRAWLER_BRANCH=$KRAWLER_BRANCH -f dockerfile.arpege -t weacast/weacast-arpege .
 docker tag weacast/weacast-arpege weacast/weacast-arpege:$VERSION
-docker build -f dockerfile.gfs -t weacast/weacast-gfs .
+docker build --build-arg KRAWLER_BRANCH=$KRAWLER_BRANCH -f dockerfile.gfs -t weacast/weacast-gfs .
 docker tag weacast/weacast-gfs weacast/weacast-gfs:$VERSION
 docker login -u="$DOCKER_USER" -p="$DOCKER_PASSWORD"
 docker push weacast/weacast-arpege:$VERSION
