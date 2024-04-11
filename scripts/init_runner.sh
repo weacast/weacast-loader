9d1f37965f701474fcfe38cfb6cfeb14dd8c4e47
Luc Claustres <luc.claustres@orange.fr>
2024-04-11 09:22:09 +0200

    wip: Refactor CI to use GitHub actions and generic bash scripts (#64)

--- scripts/init_runner.sh
+++ scripts/init_runner.sh
@@ -16,11 +16,11 @@ init_github_run_tests() {
     install_reqs yq age sops nvm node16 node18 node20 mongo4 mongo5 mongo6 cc_test_reporter
 }
 
-init_github_build_models() {
+init_github_build_model() {
     install_reqs age sops nvm node16
 }
 
-init_github_build_loaders() {
+init_github_build_loader() {
     install_reqs age sops nvm node16
 }
 
